f9ea037e4b8d417fc55f299c0e314b4425b62cc5
Anass N'DRI <anassndri@live.fr>
2024-07-29 16:39:55 +0200

    update backend url

--- frontend/src/app/utils/baseUrl.ts
+++ frontend/src/app/utils/baseUrl.ts
@@ -1,5 +1,5 @@
 export class Base_url {
-  // public static Url_ServBack ='https://nest-backend-my-shop-production.up.railway.app/api';
+ public static Url_ServBack ='https://nest-backend-my-shop-production.up.railway.app/api';
 
-  public static Url_ServBack = 'http://localhost:80/api';
+   // public static Url_ServBack = 'http://localhost:80/api';
 }
